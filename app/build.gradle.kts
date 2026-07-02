d8cef3c6c38b0488fd8a891de683823bb6c2ac98
j-sun <j-sun@esol.co.jp>
2026-07-02 15:25:16 +0900

    The third test for git.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.pokemonojt03"
+        applicationId = "com.example.pokemonojt04"
         minSdk = 36
         targetSdk = 36
         versionCode = 1
